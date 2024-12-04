375e5cc05fe2ad5745ad64fa0200240d42b7f930
A4RMIN <amoraghah@gmail.com>
2024-12-04 14:55:49 +0330

    add help func and more cases

--- task-tracker.js
+++ task-tracker.js
@@ -4,6 +4,24 @@ const args = process.argv.slice(2);
 
 const command = args[0];
 
+// func to show help
+const displayHelp = () => {
+    console.log(`
+        Task Tracker CLI:
+        -----------------
+        Available commands:
+        - add <task_description>: Add a new task
+        - list: List all tasks
+        - delete <task_id>: Delete a task by ID
+        - mark-done <task_id>: Mark a task as done
+        - mark-undone <task_id>: Mark a task as undone
+        - mark-in-progress <task_id>: Mark a task as in-progress
+        - todo: Display tasks to do
+        - help: Show this help message
+        `);
+};
+
+// command handlers
 switch (command) {
     case "add":
         console.log("Adding a task ...");
@@ -14,8 +32,20 @@ switch (command) {
     case "delete":
         console.log("Deleting a task ...");
         break;
+    case "mark-done":
+        console.log("Marking a task as done ...");
+        break;
+    case "mark-undone":
+        console.log("Marking a task as undone ...");
+        break;
+    case "todo":
+        console.log("Displaying tasks to do ...");
+        break;
+    case "mark-in-progress":
+        console.log("Marking a task as in-progress...");
+        break;
     case "help":
-        console.log("Available commands: add, list, delete, help");
+        displayHelp();
         break;
     default:
         console.log("Unknown command, Please use help for available commands");
